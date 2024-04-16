6e0c9f15954a89f126c56ddcb3c3799502ac8b08
Jellis Desktop <unconfigured@null.spigotmc.org>
2024-04-16 17:08:07 +0200

    Add project files.

---
+++ Data/Models/WordPair.cs
@@ -0,0 +1,16 @@
+﻿namespace WordGenerator.Data.Models
+{
+	internal class WordPair
+	{
+		public string FullWord { get { return FirstHalf + LastHalf; } }
+
+		public string FirstHalf { get; set; }
+		public string LastHalf { get; set; }
+
+		public WordPair(string firstHalf, string lastHalf)
+		{
+			FirstHalf = firstHalf;
+			LastHalf = lastHalf;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using WordGenerator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WordGenerator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string filePath = "C:\\Users\\Jellis\\source\\repos\\WordGenerator\\Data\\input.txt";
+			HashSet<string> data = new HashSet<string>(File.ReadAllLines(filePath));
+			List<string> fullWords = new List<string>();
+			List<string> halves = new List<string>();
+			List<WordPair> wordPairs = new List<WordPair>();
+
+			foreach (string word in data)
+			{
+				string trimmedWord = word.Trim();
+				if (trimmedWord.Length < 6)
+				{
+					halves.Add(trimmedWord);
+				}
+				else
+				{
+					fullWords.Add(trimmedWord);
+				}
+			};
+			for (int i = 0; i < halves.Count; i++)
+			{
+				for (int j = i + 1; j < halves.Count; j++)
+				{
+					if (fullWords.Contains(halves[i] + halves[j]))
+						wordPairs.Add(new WordPair(halves[i], halves[j]));
+				}
+			}
+			foreach (WordPair word in wordPairs.Distinct().ToList())
+			{
+				Console.WriteLine($"{word.FirstHalf}+{word.LastHalf}={word.FullWord}");
+			}
+
+		}
+	}
+}
